9b8a2c3f9d427c39115834e7d1e36b4f30f562af
zwm <beetaa@outlook.com>
2017-11-08 23:39:25 +0800

    update

---
+++ pages/ui/animation/animation.js
@@ -0,0 +1,77 @@
+// pages/ui/animation/animation.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    animationData: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    var animation = wx.createAnimation({
+      duration: 500,
+      transformOrigin: "50% 50%",
+      timingFunciton: 'ease',
+      delay: 0
+    })
+    this.animation = animation
+    animation.scale(2, 2).rotate(45).step()
+    animation.scale(1, 1).rotate(0).step()
+    this.setData({
+      animationData: animation.export()
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
